1dd801084a3d4c5a4130b31f348e847db6563d5f
Amol <amol.bidrolkar@gmail.com>
2017-03-02 11:46:12 +0300

    added string replace code below

--- HelloWorld/src/main/java/com/kdp/java/HelloWorld.java
+++ HelloWorld/src/main/java/com/kdp/java/HelloWorld.java
@@ -1,5 +1,5 @@
 package com.kdp.java;
-
+import java.util.regex.Pattern;
 public class HelloWorld {
 
 	/**
@@ -8,6 +8,23 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!!!");
+		// String replace code below
+		
+		String s="AAAAaaaaabbadadadpsdappPPeeasas";
+				String S2="ABCPQR";
+			    String aa=Greeting.class.getSimpleName()      ;
+			    System.out.println("The Class name executing this is:"+aa);
+			    System.out.println("The Class name executing this is:"+Greeting.class.getName());
+				System.out.println("The String 1 : "+s+" \n The String to be replace by blank is : "+S2);
+				
+				String regex="["+S2.toLowerCase()+S2.toUpperCase()+"]";
+				
+				Pattern p=Pattern.compile(regex);
+				Matcher m=p.matcher(s);
+				String replace=m.replaceAll("");
+
+				System.out.println("The String after replacement is : "+ replace);
+		
 	}
 
 }
