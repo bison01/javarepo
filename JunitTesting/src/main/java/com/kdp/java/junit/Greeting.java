4c5efa81ed1f9f1c2bc3714723a80dbb6db68a35
Amol <amol.bidrolkar@gmail.com>
2017-03-02 11:40:18 +0300

    added match method

--- JunitTesting/src/main/java/com/kdp/java/junit/Greeting.java
+++ JunitTesting/src/main/java/com/kdp/java/junit/Greeting.java
@@ -1,11 +1,40 @@
 package com.kdp.java.junit;
+import java.util.regex.Pattern;
 
+
+
+import java.util.regex.Matcher;
 public class Greeting {
-
+	
 	public String greet(String str) {
 		if (str == null || str.length() ==0){
 			throw new IllegalArgumentException();
 		}
 		return "Hello " + str;
+	
 	}
+	
+	
+	public void match()
+	{
+		//next line of code to test 
+				String s="AAAAaaaaabbadadadpsdappPPeeasas";
+				String S2="ABCPQR";
+			    String aa=Greeting.class.getSimpleName()      ;
+			    System.out.println("The Class name executing this is:"+aa);
+			    System.out.println("The Class name executing this is:"+Greeting.class.getName());
+				System.out.println("The String 1 : "+s+" \n The String to be replace by blank is : "+S2);
+				
+				String regex="["+S2.toLowerCase()+S2.toUpperCase()+"]";
+				
+				Pattern p=Pattern.compile(regex);
+				Matcher m=p.matcher(s);
+				String replace=m.replaceAll("");
+
+				System.out.println("The String after replacement is : "+ replace);
+				
+			
+	}
+	
+	
 }
